f8f27296083bc9417aafdb1b27777ad298875029
Keerthi96akarapu <113994505+Keerthi96akarapu@users.noreply.github.com>
2023-04-03 16:31:25 +0530

    Add files via upload

---
+++ consumer/src/main/java/com/spring/cloud/feignclient/service/BookRestConsumer.java
@@ -0,0 +1,26 @@
+package com.spring.cloud.feignclient.service;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.spring.cloud.feignclient.model.Book;
+
+@FeignClient(name="BOOK-SERVICE")
+public interface BookRestConsumer {
+	 @GetMapping("/book/data")
+     public String getBookData();
+
+     @GetMapping("/book/{id}")
+     public Book getBookById(@PathVariable Integer id);
+
+     @GetMapping("/book/all")
+     public List<Book> getAllBooks();
+
+     @GetMapping("/book/entity")
+     public ResponseEntity<String> getEntityData();
+
+}
